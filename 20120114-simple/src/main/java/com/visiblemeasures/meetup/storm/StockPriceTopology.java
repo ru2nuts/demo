3e9a7c06e31a9cab97f507d1b7fe218ebcf5393b
Alex Arutyunyants <aarutyunyants@visiblemeasures.com>
2013-01-14 15:41:42 -0500

    clean up

--- 20120114-simple/src/main/java/com/visiblemeasures/meetup/storm/StockPriceTopology.java
+++ 20120114-simple/src/main/java/com/visiblemeasures/meetup/storm/StockPriceTopology.java
@@ -9,15 +9,9 @@ public class StockPriceTopology {
   public static void main(String[] args) {
     TopologyBuilder builder = new TopologyBuilder();
     builder.setSpout("stocks_in", new YqlJsonSpout(), 1);
-    builder.setBolt("stocks_print", new StockPricePrintBolt(), 1).shuffleGrouping("stocks_in");
+    builder.setBolt("stocks_print", new StockPricePrintBolt(), 3).shuffleGrouping("stocks_in");
 
     Config conf = new Config();
-    conf.setDebug(false);
-    conf.setNumAckers(2);
-    conf.setNumWorkers(2);
-    conf.setMaxTaskParallelism(4);
-    conf.setMaxSpoutPending(100);
-    conf.setMessageTimeoutSecs(30);
 
     LocalCluster cluster = new LocalCluster();
     cluster.submitTopology("TickerTopology", conf, builder.createTopology());
